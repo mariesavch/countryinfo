0521150675b9eae76bfe56c29df8915baeff6701
mariesavch <mariesavch@icloud.com>
2024-11-04 16:04:44 +0500

    feat: tailwind merge

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use dioxus_logger::tracing::Level;
 use dioxus_sdk::storage::*;
 use num_format::{Locale, ToFormattedString};
 use serde::Deserialize;
+use tailwind_fuse::tw_merge;
 
 const _TAILWIND_URL: &str = manganis::mg!(file("assets/tailwind.css"));
 
@@ -72,9 +73,10 @@ async fn get_countryinfo(location: String) -> reqwest::Result<Vec<CountryData>> 
 #[component]
 fn App() -> Element {
     let mut country =
-        use_synced_storage::<LocalStorage, String>("country".to_string(), || "Austria".to_string());
+        use_synced_storage::<LocalStorage, String>("country".to_string(), || "".to_string());
     let countryinfo =
         use_resource(move || async move { get_countryinfo(country.to_string()).await });
+    let item = "flex py-3 flex-col gap-1";
 
     rsx! {
         main { class: "mx-auto max-w-[850px] px-6 pb-20",
@@ -87,47 +89,48 @@ fn App() -> Element {
                     r#type: "text",
                     autofocus: true,
                     oninput: move |event| country.set(event.value()),
-                    class: "rounded-md border border-surface0 bg-base
-                            py-2 px-4 capitalize shadow-sm
-                            outline-none transition-colors duration-300
-                            placeholder:text-overlay0 hover:border-surface1
-                            focus:text-text focus:border-surface2"
+                    class: tw_merge!(
+                        "rounded-md border border-surface0 bg-base py-2 px-4 capitalize",
+                        "outline-none transition-colors duration-300",
+                        "placeholder:text-overlay0 hover:border-surface1",
+                        "focus:text-text focus:border-surface2"
+                    )
                 }
                 div { class: "mt-6",
                     if let Some(Ok(data)) = countryinfo.read().as_ref() {
                         ul { class: "animated-list grid grid-cols-1 sm:grid-cols-2 mt-5",
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Official name" }
                                     span { "{data[0].name.official} {data[0].flag}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Capital" }
                                     span { "{data[0].capital[0]}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Region" }
                                     span { "{data[0].region}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Subregion" }
                                     span { "{data[0].subregion}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "LatLng" }
                                     span { "{data[0].latlng[0]}/{data[0].latlng[1]}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Capital LatLng" }
                                     span {
                                         "{data[0].capital_info.latlng[0]}/{data[0].capital_info.latlng[0]}"
@@ -135,7 +138,7 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Timezones" }
                                     span {
                                         {data[0].timezones.iter().map(|timezone|
@@ -146,7 +149,7 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "TLD" }
                                     span {
                                         {data[0].tld.iter().map(|tld|
@@ -157,13 +160,13 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Population" }
                                     span { "{data[0].population.to_formatted_string(&Locale::en)}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Borders" }
                                     span {
                                         if let Some(borders) = &data[0].borders {
@@ -175,7 +178,7 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Languages" }
                                     span {
                                         {data[0].languages.values().map(|lang|
@@ -186,7 +189,7 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Currencies" }
                                     span {
                                         {data[0].currencies.values().map(|currency|
@@ -197,7 +200,7 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Landlocked" }
                                     if data[0].landlocked {
                                         span { "Yes" }
@@ -207,13 +210,13 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Start of week" }
                                     span { class: "capitalize", "{data[0].startOfWeek}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Continents" }
                                     span {
                                         if let Some(continents) = &data[0].continents {
@@ -223,7 +226,7 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Maps" }
                                     span {
                                         a {
