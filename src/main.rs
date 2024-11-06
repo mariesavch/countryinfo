15d553684025b419e4ee94bae319a2eb5f1201bc
mariesavch <mariesavch@icloud.com>
2024-11-06 21:00:08 +0500

    dioxus 0.6

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,6 @@ use dioxus_logger::tracing::Level;
 use dioxus_sdk::storage::*;
 use num_format::{Locale, ToFormattedString};
 use serde::Deserialize;
-use tailwind_fuse::tw_merge;
-
-const _TAILWIND_URL: &str = manganis::mg!(file("assets/tailwind.css"));
 
 fn main() {
     dioxus_sdk::storage::set_dir!();
@@ -76,12 +73,20 @@ fn App() -> Element {
         use_synced_storage::<LocalStorage, String>("country".to_string(), || "".to_string());
     let countryinfo =
         use_resource(move || async move { get_countryinfo(country.to_string()).await });
-    let item = "flex py-3 flex-col gap-1";
 
     rsx! {
-        main { class: "mx-auto max-w-[850px] px-6 pb-20",
+        document::Link { rel: "stylesheet", href: asset!("/assets/main.css") }
+        main {
+            margin_left: "auto",
+            margin_right: "auto",
+            max_width: "768px",
+            padding_top: "64px",
+            padding_left: "24px",
+            padding_right: "24px",
+            padding_bottom: "80px",
             div { class: "pt-6 min-[950px]:pt-16",
                 input {
+                    class: "input",
                     aria_label: "Enter country",
                     placeholder: "Enter country",
                     spellcheck: false,
@@ -89,88 +94,86 @@ fn App() -> Element {
                     r#type: "text",
                     autofocus: true,
                     oninput: move |event| country.set(event.value()),
-                    class: tw_merge!(
-                        "rounded-md border border-surface0 bg-base py-2 px-4 capitalize",
-                        "outline-none transition-colors duration-300",
-                        "placeholder:text-overlay0 hover:border-surface1",
-                        "focus:text-text focus:border-surface2"
-                    )
                 }
-                div { class: "mt-6",
+                div { margin_top: "12px",
                     if let Some(Ok(data)) = countryinfo.read().as_ref() {
-                        ul { class: "animated-list grid grid-cols-1 sm:grid-cols-2 mt-5",
+                        ul {
+                            class: "animated-list",
+                            all: "unset",
+                            display: "grid",
+                            grid_template_columns: "repeat(auto-fit, minmax(300px, 1fr))",
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Official name" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Official name" }
                                     span { "{data[0].name.official} {data[0].flag}" }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Capital" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Capital" }
                                     span { "{data[0].capital[0]}" }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Region" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Region" }
                                     span { "{data[0].region}" }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Subregion" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Subregion" }
                                     span { "{data[0].subregion}" }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "LatLng" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "LatLng" }
                                     span { "{data[0].latlng[0]}/{data[0].latlng[1]}" }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Capital LatLng" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Capital LatLng" }
                                     span {
                                         "{data[0].capital_info.latlng[0]}/{data[0].capital_info.latlng[0]}"
                                     }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Timezones" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Timezones" }
                                     span {
-                                        {data[0].timezones.iter().map(|timezone|
-                                        rsx!(
-                                            span {"{timezone} "}
-                                        ))}
+                                        {data[0].timezones.iter().map(|timezone| rsx! {
+                                            span { "{timezone} " }
+                                        })}
                                     }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "TLD" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "TLD" }
                                     span {
-                                        {data[0].tld.iter().map(|tld|
-                                        rsx!(
-                                            span {"{tld} "}
-                                        ))}
+                                        {data[0].tld.iter().map(|tld| rsx! {
+                                            span { "{tld} " }
+                                        })}
                                     }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Population" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Population" }
                                     span { "{data[0].population.to_formatted_string(&Locale::en)}" }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Borders" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Borders" }
                                     span {
                                         if let Some(borders) = &data[0].borders {
-                                            { borders.iter().map(|border| rsx!(span {"{border} "})) }
+                                            {borders.iter().map(|border| rsx! {
+                                                span { "{border} " }
+                                            })}
                                         } else {
                                             span { "None" }
                                         }
@@ -178,30 +181,28 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Languages" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Languages" }
                                     span {
-                                        {data[0].languages.values().map(|lang|
-                                        rsx!(
-                                            span {"{lang} "}
-                                        ))}
+                                        {data[0].languages.values().map(|lang| rsx! {
+                                            span { "{lang} " }
+                                        })}
                                     }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Currencies" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Currencies" }
                                     span {
-                                        {data[0].currencies.values().map(|currency|
-                                        rsx!(
-                                            span {"{currency.name} ({currency.symbol}) "}
-                                        ))}
+                                        {data[0].currencies.values().map(|currency| rsx! {
+                                            span { "{currency.name} ({currency.symbol}) " }
+                                        })}
                                     }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Landlocked" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Landlocked" }
                                     if data[0].landlocked {
                                         span { "Yes" }
                                     } else {
@@ -210,24 +211,26 @@ fn App() -> Element {
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Start of week" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Start of week" }
                                     span { class: "capitalize", "{data[0].startOfWeek}" }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Continents" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Continents" }
                                     span {
                                         if let Some(continents) = &data[0].continents {
-                                            { continents.iter().map(|continent| rsx!(span {"{continent} "})) }
+                                            {continents.iter().map(|continent| rsx! {
+                                                span { "{continent} " }
+                                            })}
                                         }
                                     }
                                 }
                             }
                             li {
-                                div { class: item,
-                                    span { class: "text-overlay0", "Maps" }
+                                div { class: "item",
+                                    span { color: "var(--overlay0)", "Maps" }
                                     span {
                                         a {
                                             class: "underlined",
