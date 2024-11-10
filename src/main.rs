1bf04175821cd097861e7b621e154614ec494dc5
mariesavch <mariesavch@icloud.com>
2024-11-10 15:35:45 +0500

    fix font

--- src/main.rs
+++ src/main.rs
@@ -131,6 +131,7 @@ make_styles! {
             background_color: "var(--base)",
             color: "var(--text)",
             line_height: "1.6",
+            font_family: "Cartograph CF",
         },
         "@media (hover: hover) and (pointer: fine)" {
             ".animated_list li" {
@@ -214,6 +215,19 @@ fn App() -> Element {
         use_resource(move || async move { get_countryinfo(country.to_string()).await });
 
     rsx! {
+        style {
+            "
+            @font-face {{
+            font-family: 'Cartograph CF';
+            src: url('./assets/fonts/CartographCF-Regular.woff2') format('woff2');
+            }}
+
+            @font-face {{
+            font-family: 'Cartograph CF';
+            src: url('./assets/fonts/CartographCF-Bold.woff2') format('woff2');
+            font-weight: bold;
+            }}"
+        }
         main {
             margin_left: "auto",
             margin_right: "auto",
